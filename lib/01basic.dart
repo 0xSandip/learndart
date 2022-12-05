5f5527625ee520ca5c9eeabe656976b89f2db6a0
0xSandip <sandp253649@gmail.com>
2022-12-05 08:58:52 +0545

    basics done

---
+++ lib/01basic.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  print('welcome to dart');
+  stdout.write("Enter your name");
+
+  var name = stdin.readLineSync();
+  print("Welcome, $name");
+
+  // Takes the input from the console and takes it in string form.
+  // $ concatinates
+}
+
+//just returns a void/empty function
